1aa7728cd9addb58115869824544c415df764c45
t-elieta <elieta.takou@gmail.com>
2026-03-24 17:03:06 +0100

    Add home.html and display books from populated database

--- Leselystapp/views.py
+++ Leselystapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Books
 
-# Create your views here.
+def home(request):
+    books = Books.objects.all()
+    return render(request, 'home.html', {'books': books})
