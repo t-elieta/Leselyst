41a4bc5d827025ad4d017c47bab3cf5841bb1b4f
t-elieta <elieta.takou@gmail.com>
2026-04-30 12:57:31 +0200

    Implement basic views for the application

--- Leselystapp/views.py
+++ Leselystapp/views.py
@@ -4,3 +4,19 @@ from .models import Books
 def home(request):
     books = Books.objects.all()
     return render(request, 'home.html', {'books': books})
+
+def book_list(request):
+    books = Books.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
+def book_detail(request, book_id):
+    book = Books.objects.get(id=book_id)
+    return render(request, 'book_detail.html', {'book': book})
+
+def author_list(request):
+    authors = Books.objects.values_list('author', flat=True).distinct()
+    return render(request, 'author_list.html', {'authors': authors})
+
+def author_detail(request, author_name):
+    books = Books.objects.filter(author=author_name)
+    return render(request, 'author_detail.html', {'books': books})
