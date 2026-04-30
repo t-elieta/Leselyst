baaf2105c462d859daa3ed83b0dbc19ac5157209
t-elieta <elieta.takou@gmail.com>
2026-04-30 13:28:57 +0200

    Add templates

--- Leselyst/settings.py
+++ Leselyst/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+AUTH_USER_MODEL = 'Leselystapp.User'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
